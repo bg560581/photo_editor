dfe200825348d5a3f31d1c1771faf33a3b8c2994
bg560581 <bennett.galgano@gmail.com>
2023-04-05 01:12:14 -0400

    front and backend are connected working on showing the data

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-// const methodOverride = require('method-override')
 const app = express()
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("Mongoose connected", process.env.MONGO_URI))
+const cors = require('cors');
 
+
+//middleware
+app.use(cors());
 
 //controllers and routes
 app.use('/photos', require('./controllers/photos'))
